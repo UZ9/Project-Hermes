82f69f32e6b4fca6504d827b977e6b1bb1212913
Yerti <36551149+UZ9@users.noreply.github.com>
2021-11-20 21:56:59 -0600

    Add automatic fetching of skills scores from a targeted SKU code event

---
+++ fetch.py
@@ -0,0 +1,84 @@
+import requests
+import json
+import time
+import yaml
+
+credentials = yaml.load(open('./config.yml'))
+
+authKey = credentials['credentials']['apiKey']
+
+# Given as "Event Code" on the page's website
+skuId = "RE-VRC-21-5182"
+
+# Season ID
+# 154 - 2021 Tipping Point
+# (Found using the /seasons API call)
+seasonId = "154"
+
+# Robot events api url to pull from
+baseApiUrl = "https://www.robotevents.com/api/v2/"
+
+baseHeader = {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer " + authKey
+}
+
+# Fetch Event ID using SKU
+eventIdResponse = requests.get(
+    baseApiUrl + "seasons/" + seasonId + "/events?sku=RE-VRC-21-5182", headers=baseHeader)
+
+# Parse eventId from response
+eventId = eventIdResponse.json()['data'][0]['id']
+
+# Fetch list of teams from event
+teamResponse = requests.get(
+    baseApiUrl + "events/" + str(eventId) + "/teams", headers=baseHeader)
+
+
+# Parse json to get teamList dict
+teamList = teamResponse.json()['data']
+
+
+teamCount = len(teamList)
+
+output={}
+
+# Loop through the teams
+for teamIndex in range(1, teamCount):
+    team = teamList[teamIndex - 1]
+
+    skillsResponse = requests.get(
+    baseApiUrl + "teams/" + str(team["id"]) + "/skills?season=" + seasonId, headers=baseHeader)
+
+    skills = skillsResponse.json();
+
+
+    topDrivingSkills = 0
+    topAutonSkills = 0
+
+    for event in skills["data"]:
+        if event["type"] == "driver":
+            if event["score"] > topDrivingSkills:
+                topDrivingSkills = event["score"]
+        if event["type"] == "programming":
+            if event["score"] > topAutonSkills:
+                topAutonSkills = event["score"]
+
+    output[str(team["team_name"])] = {
+        "number": team["number"],
+        "organization": team["organization"],
+        "skills": {
+            "driver": topDrivingSkills,
+            "programming": topAutonSkills
+        }
+    }
+
+    time.sleep(0.1)
+    print("Processing team (" + str(teamIndex) + "/" + str(teamCount) + ")", end="\r")
+
+print("Processing team (15/15)")
+print("\n")
+print("==============================================")
+    
+print(json.dumps(output, indent=4))
+# print(teamResponse.json())
