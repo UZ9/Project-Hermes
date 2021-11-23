596ff37a602e770748cf3876465284f81ac3ed76
Yerti <chessboy5@icloud.com>
2021-11-23 10:20:43 -0600

    :sparkles: Allow label to be pressed to choose Check in Scout Form
    - This certainly makes it easier to select a value on mobile, as there is no longer one tiny spot the user has to press.
    - Add more questions to the form 
    - Add a submit button to the form (Logic WiP)

--- src/components/ScoutFormComponent.js
+++ src/components/ScoutFormComponent.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useParams } from "react-router-dom"
 import { Form } from "react-bootstrap";
+import Button from "@restart/ui/esm/Button";
+import "../App.css"
 
 function ScoutFormComponent() {
     let { id } = useParams();
@@ -14,19 +16,33 @@ function ScoutFormComponent() {
                             <Form.Label>Autonomous Consistency </Form.Label>
                             <Form.Range min={0} max={5} />
                         </Form.Group>
-                        <Form.Group>
-                            <Form.Check style={{width: "200%"}} type="checkbox" label="Can move mobile goals" />
+                        <Form.Group className="mt-4" controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="Can move mobile goals" />
                         </Form.Group>
-                        <Form.Group>
+                        <Form.Group className="mt-4" controlId="formBasicCheckbox2">
                             <Form.Check type="checkbox" label="Can place mobile goals on platforms" />
                         </Form.Group>
-                        <Form.Group>
+                        <Form.Group className="mt-4" controlId="formBasicCheckbox3">
                             <Form.Check type="checkbox" label="Can place yellow mobile yellows on platform" />
-                        </Form.Group>
-                        <Form.Group>
+                        </Form.Group >
+                        <Form.Group className="mt-4" controlId="formBasicCheckbox4">
                             <Form.Check type="checkbox" label="Can move yellow mobile goals" />
                         </Form.Group>
+                        <Form.Group className="mt-4" controlId="formBasicCheckbox5">
+                            <Form.Check type="checkbox" label="Can park" />
+                        </Form.Group>
+                        <Form.Group className="mt-4" controlId="formBasicCheckbox6">
+                            <Form.Check type="checkbox" label="Can park with mobile goals" />
+                        </Form.Group>
+                        <Form.Group className="mt-4" controlId="formBasicCheckbox7">
+                            <Form.Label>Amount of mobile goals they can park with</Form.Label>
+                            <Form.Control type="text" placeholder="Put number here"/>
+                        </Form.Group>
+                        <Form.Group className="mt-4">
+                            <Button className="w-100 btn mt-3 btn-primary" type="button">Submit</Button>
+                        </Form.Group>
                     </Form>
+                    
                 </div>
             </div>
         </div>
