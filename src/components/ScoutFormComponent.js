2d1be42fa7e0b63a4b873e831bf0c406d923f278
Yerti <chessboy5@icloud.com>
2021-11-23 10:20:43 -0600

    :construction: First prototype of Scout Form
    - Remove old elements from ScoutFormComponent
    - Add a card-based layout for the Scout Form, including a few questions that will be asked during competitions

--- src/components/ScoutFormComponent.js
+++ src/components/ScoutFormComponent.js
@@ -6,16 +6,30 @@ function ScoutFormComponent() {
     let { id } = useParams();
 
     return (
-        <Form>
-            <Form.Label>Autonomous Consistency</Form.Label>
-            <Form.Range />
-            <Form.Label>Autonomous Consistency</Form.Label>
-            <Form.Range />
-            <Form.Label>Autonomous Consistency</Form.Label>
-            <Form.Range />
-            <Form.Label>Autonomous Consistency</Form.Label>
-            <Form.Range />
-        </Form>
+        <div className="col-xl-4 mx-auto col-sm-5 p-2">
+            <div className={`card card-common`}>
+                <div className="card-body">
+                    <Form>
+                        <Form.Group>
+                            <Form.Label>Autonomous Consistency </Form.Label>
+                            <Form.Range min={0} max={5} />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Check style={{width: "200%"}} type="checkbox" label="Can move mobile goals" />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Check type="checkbox" label="Can place mobile goals on platforms" />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Check type="checkbox" label="Can place yellow mobile yellows on platform" />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Check type="checkbox" label="Can move yellow mobile goals" />
+                        </Form.Group>
+                    </Form>
+                </div>
+            </div>
+        </div>
 
     )
 }
