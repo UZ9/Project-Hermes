62d5547c1c630d2b8565f4b22f8a41da0b70548c
Yerti <chessboy5@icloud.com>
2021-12-03 20:13:55 -0600

    :bug: Point socket url to backend rather than frontend

--- src/service/Socket.js
+++ src/service/Socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "https://project-hermes-frontend.herokuapp.com/";
+const SOCKET_URL = "https://project-hermes-express-backend.herokuapp.com/";
 
 export const socket = io(SOCKET_URL, {transports: ['websocket', 'polling', 'flashsocket']});
