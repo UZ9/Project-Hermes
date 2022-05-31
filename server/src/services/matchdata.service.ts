e22ce209742a8c416b98d4f6c3546caa6d67bee0
Yerti <chessboy5@icloud.com>
2022-05-31 10:32:52 -0500

    Add automatic retrieval of match data every 5 minutes

--- server/src/services/matchdata.service.ts
+++ server/src/services/matchdata.service.ts
@@ -1,10 +1,20 @@
 import { mongoService, socketServer } from "../index";
 import { Socket } from "socket.io";
 import { logger } from "../middlewares/logging.middleware";
+import * as robotevents from 'robotevents';
+import { Match } from "robotevents/out/endpoints/matches";
+import * as cliProgress from 'cli-progress'
 
 // TODO: Find a more appropriate name for this service
 export class MatchDataService {
     private readonly collectionName: string = "hermes";
+
+    constructor() {
+        // Update match data every 5 minutes
+        setInterval(() => {
+            this.fetchMatchData();
+        }, 1000 * 60 * 5);
+    }
 
     private async updateClientData() {
         // Retrieve all data from mongodb
@@ -14,9 +24,147 @@ export class MatchDataService {
         socketServer.emit("data-update", { data });
     }
 
-    // public fetchMatchData(): void {
+    private async updateServerData(newData: any) {
+        // Bulk write data to avoid unncessary write calls to mongodb
+        const bulk = []
 
-    // }
+        for (const team of newData) {
+            bulk.push({
+                updateOne: {
+                    filter: { "id": team.id },
+                    update: {
+                        $set: {
+                            id: team.id,
+                            name: team.name,
+                            organizaiton: team.organization,
+                            skills: team.skills,
+                            matches: team.matches,
+                            division: team.division
+                        }
+                    },
+                    upsert: true
+                }
+            })
+        }
+
+        await mongoService.getDb().collection('hermes').bulkWrite(bulk);
+
+        logger.debug("Finished pushing MongoDB bulk write update");
+    }
+
+    public async fetchMatchData() {
+        logger.debug("Starting match data fetch retrieval")
+
+        const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+
+        const teams = new Set();
+
+        // RobotEvents requires an API key to operate
+        robotevents.authentication.setBearer(process.env.API_KEY);
+
+
+        // Pull event data
+        const event = await robotevents.events.get(process.env.SKU);
+
+        // TODO: Don't hardcode the division index
+        const division = event.divisions[1];
+
+        const matches = await event.matches(division.id);
+
+        const matchesPerTeam = new Map<number, Match[]>();
+
+        if (teams.size === 0) {
+            matches.forEach(match => {
+                const matchTeams = [
+                    match.alliances[0].teams[0].team.id,
+                    match.alliances[0].teams[1].team.id,
+                    match.alliances[1].teams[0].team.id,
+                    match.alliances[1].teams[1].team.id
+                ]
+
+                matchTeams.forEach(t => {
+                    teams.add(t);
+
+                    ((matchesPerTeam as any)[t] = (matchesPerTeam as any)[t] || []).push(match);
+                })
+            })
+        }
+
+        const teamObjects = [...(await event.teams()).entries()].filter(t => teams.has(t[0])).map(t => t[1]);
+
+        const data: any = []
+
+        bar1.start(teamObjects.length, 0);
+
+
+        teamObjects.forEach((team, index) => {
+            setTimeout(() => {
+
+                team.skills({ season: [robotevents.seasons.current("VRC")] }).then(skills => {
+                    const allSkills = skills.array();
+
+                    const programming = allSkills.filter(sk => sk.type === 'programming').reduce((a, b) => a.score > b.score ? a : b).score;
+                    const driver = allSkills.filter(sk => sk.type === 'driver').reduce((a, b) => a.score > b.score ? a : b).score;
+
+                    const teamMatches = (matchesPerTeam as any)[teamObjects[index].id] as Match[]
+
+                    const ranking = teamObjects[index].rankings({ event: [event.id] }).then(r => {
+                        const currentRank = r.array().length > 0 ? r.array()[0] : { rank: 0, wins: 0, losses: 0, ties: 0, wp: 0, ap: 0, sp: 0, high_score: 0 };
+
+                        data.push({
+                            name: teamObjects[index].team_name,
+                            id: teamObjects[index].number,
+                            organization: teamObjects[index].organization,
+                            skills: {
+                                "driver": driver,
+                                "programming": programming,
+                                "world-rank": "0"
+                            },
+                            division: {
+                                "ranking": currentRank.rank,
+                                "wins": currentRank.wins,
+                                "losses": currentRank.losses,
+                                "ties": currentRank.ties,
+                                "wp": currentRank.wp,
+                                "ap": currentRank.ap,
+                                "sp": currentRank.sp,
+                                "high_score": currentRank.high_score,
+                                "average_points": -1,
+                                "total_points": -1,
+                            },
+                            matches: teamMatches.map(match => [match.name, {
+                                "red-alliance": {
+                                    score: match.alliances[0].score,
+                                    teams: match.alliances[0].teams.map(t => t.team.name)
+                                },
+                                "blue-alliance": {
+                                    score: match.alliances[1].score,
+                                    teams: match.alliances[1].teams.map(t => t.team.name)
+
+                                }
+                            }])
+                        })
+
+                        bar1.increment();
+                        if (bar1.getProgress() === teamObjects.length) {
+                            bar1.stop();
+
+                            this.updateServerData(data);
+
+                        }
+
+                    })
+
+
+
+
+                }).catch(e => {
+                    logger.error(e)
+                })
+            }, 0.8 * 1000 * index);
+
+        })
+    }
 
     public connect(socket: Socket): void {
         logger.debug("New Socket.IO Connection received");
